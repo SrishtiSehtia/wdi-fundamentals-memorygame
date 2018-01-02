58b95dc9abdc5d524b7a0276b116d56d07bd0170
Srishti Sehtia <srishti0123@gmail.com>
2018-01-02 10:45:27 -0800

    store cards in array and add conditional statement for game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipepr" + cardOne);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardTwo)
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("Youe found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
