ddf886b4b41658bfe1a54e4a3b2d1c8ea2318504
Srishti Sehtia <srishti0123@gmail.com>
2018-01-06 10:33:15 -0800

    create object for each card

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 
 var checkForMatch = function () {
@@ -14,8 +35,8 @@ var flipCard = function (cardId) {
 	cardsInPlay.push(cards[cardId]);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
-	};
+	}
 };
 
 flipCard(0);
-// flipCard(2);
+flipCard(2);
